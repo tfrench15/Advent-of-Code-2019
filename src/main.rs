d5d2b7eed6c63417f1a2173954d1876f8d7f87dd
Tim French <tfrench15@gmail.com>
2019-12-06 15:18:00 -0800

    day one

---
+++ src/main.rs
@@ -0,0 +1,12 @@
+
+mod day_one;
+
+fn main() {
+    let answer_one = day_one::part_one();
+
+    let answer_two = day_one::part_two();
+
+    println!("{}", answer_one);
+    println!("{}", answer_two);
+}
+
