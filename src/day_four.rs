b1b874464426342ce0b462396f94bac6d1ad84d3
Tim French <tfrench15@gmail.com>
2019-12-08 14:39:58 -0600

    stub out day 4, part 1

---
+++ src/day_four.rs
@@ -0,0 +1,24 @@
+
+
+const RANGE: (i32, i32) = (284639, 748759);
+
+pub fn part_four() -> i32 {
+
+}
+
+fn is_double(num: i32) -> bool {
+    // TODO
+}
+
+fn is_increasing(num: i32) -> bool { 
+    // TODO    
+}
+
+fn num_to_str(num: i32) -> String {
+    // TODO
+}
+
+fn str_to_num(num: &str) -> i32 {
+    // TODO 
+}
+
